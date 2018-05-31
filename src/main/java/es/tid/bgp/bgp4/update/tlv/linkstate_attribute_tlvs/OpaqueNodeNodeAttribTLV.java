ba9a805cafa47024f64b94d3496db3c060f4faf7
Andrea Sgambelluri <andrea.sgambelluri@gmail.com>
2018-05-31 11:40:10 +0200

    cc

--- src/main/java/es/tid/bgp/bgp4/update/tlv/linkstate_attribute_tlvs/OpaqueNodeNodeAttribTLV.java
+++ src/main/java/es/tid/bgp/bgp4/update/tlv/linkstate_attribute_tlvs/OpaqueNodeNodeAttribTLV.java
@@ -27,10 +27,11 @@ public class OpaqueNodeNodeAttribTLV extends BGP4TLVFormat{
 	@Override
 	public void encode() {
 		int len= 0;
-		System.out.println("number of entries is "+String.valueOf(this.neighbours));
+		System.out.println("neighbours list is "+String.valueOf(this.neighbours));
 
 		if (this.neighbours!= null) {
 			len=neighbours.size()*8;
+			System.out.println("len is: "+String.valueOf(len));
 		}
 		this.setTLVValueLength(len);
 		//this.setTlv_bytes(new byte[this.getTotalTLVLength()]);
@@ -57,7 +58,7 @@ public class OpaqueNodeNodeAttribTLV extends BGP4TLVFormat{
 		if (this.tlv_bytes==null) {
 			return;
 		}
-		int num_neigh=(length-offset)/8;
+		int num_neigh=(length)/8;
 		System.out.println("number of entries is "+String.valueOf(num_neigh));
 		for (int i=0; i< num_neigh; i++){
 			Inet4Address as =null;
